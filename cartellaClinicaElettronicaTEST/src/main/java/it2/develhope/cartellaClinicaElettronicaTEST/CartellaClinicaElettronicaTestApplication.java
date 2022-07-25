d2de2b760a7560e47be533f87951e7b9767f929d
AngeloZammataro <100445771+AngeloZammataro@users.noreply.github.com>
2022-07-25 09:42:14 +0200

    First Commit TEST

---
+++ cartellaClinicaElettronicaTEST/src/main/java/it2/develhope/cartellaClinicaElettronicaTEST/CartellaClinicaElettronicaTestApplication.java
@@ -0,0 +1,13 @@
+package it2.develhope.cartellaClinicaElettronicaTEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartellaClinicaElettronicaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartellaClinicaElettronicaTestApplication.class, args);
+	}
+
+}
---
+++ cartellaClinicaElettronicaTEST/src/test/java/it2/develhope/cartellaClinicaElettronicaTEST/CartellaClinicaElettronicaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package it2.develhope.cartellaClinicaElettronicaTEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartellaClinicaElettronicaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
